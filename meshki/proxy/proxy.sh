ab37a3ff69a865a42476a0ec9d17114c39d845cc
abalfazlmeshki <103041670+abalfazlmeshki@users.noreply.github.com>
2022-04-06 09:46:14 +0300

    Update proxy.sh

--- meshki/proxy/proxy.sh
+++ meshki/proxy/proxy.sh
@@ -6,8 +6,6 @@ certbot certonly --standalone -d www.meshki.me -m abolfazlmeshki@gmail.com --agr
 #copy the certificate and the key for NGINX
 cp -f /etc/letsencrypt/live/www.meshki.me/fullchain.pem /etc/nginx/ssl
 cp -f /etc/letsencrypt/live/www.meshki.me/privkey.pem /etc/nginx/ssl
-#rm -f /etc/letsencrypt/live/www.meshki.me/fullchain.pem
-#rm -f /etc/letsencrypt/live/www.meshki.me/privkey.pem
 
 # start the NGINX
 nginx -g "daemon off;"
